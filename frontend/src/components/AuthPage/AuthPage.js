8798cce8a2b5f667feed762d1c8d5593e79da860
ahong14 <ahong@ucdavis.edu>
2019-03-25 22:39:10 -0700

    fixing proxy route for href

--- frontend/src/components/AuthPage/AuthPage.js
+++ frontend/src/components/AuthPage/AuthPage.js
@@ -4,12 +4,26 @@ import './AuthPage.css';
 
 //Component to display login page
 class AuthPage extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            currentURL: window.location.href
+        }
+    }
+
+    componentDidMount(){
+        var authURL = this.state.currentURL + '/api/auth';
+        this.setState({
+            currentURL: authURL
+        })
+    }
+    
     render(){
         return(
             <div className = "container-fluid content">
                 <div id = "loginContent">
                     <img id = "spotifyLogo" src = "https://i.pinimg.com/originals/a7/26/44/a726446dc5df987cd403be79db247ab7.png" alt = "spotify logo"/>
-                    <a id = "loginButton" href = "http://localhost:4000/api/auth"> <button  type="button" className="btn btn-primary btn-lg">Login</button> </a>
+                    <a id = "loginButton" href = {this.state.currentURL}> <button  type="button" className="btn btn-primary btn-lg">Login</button> </a>
                 </div>
             </div>
         );
